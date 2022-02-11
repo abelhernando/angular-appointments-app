68f81c318c1b8637a4991d64677bab9744585b48
abel <abel.hernando@basetis.com>
2022-02-11 16:31:50 +0100

    feat: footer - create core modulefooter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     AppointmentsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
