d0cd9d6a21b4b55ebda7824fe9cb12a9e2fb01bc
abel <abel.hernando@basetis.com>
2022-02-13 16:45:42 +0100

    feat: data pipe by today and tomorrow

--- src/app/shared/pipes/day.datepipe.ts
+++ src/app/shared/pipes/day.datepipe.ts
@@ -6,9 +6,14 @@ import { DatePipe } from '@angular/common';
 })
 export class DayPipe extends DatePipe implements PipeTransform {
   transform(value: Date): string {
-    console.log('~ value', value);
-    if (new Date(value).getDate() === new Date().getDate()) {
+    const targetDate = new Date(value).getDate();
+    const today = new Date().getDate();
+    const isTomorrow = targetDate  === today + 1
+
+    if (targetDate === today) {
       return 'Today';
+    } else if (isTomorrow) {
+      return 'Tomorrow';
     }
 
     return super.transform(value, 'EE');
